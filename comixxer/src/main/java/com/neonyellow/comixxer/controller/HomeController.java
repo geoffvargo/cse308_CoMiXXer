aa661826a595789609a2b26e54a1120da0289cf1
iflam <34324129+iflam@users.noreply.github.com>
2019-04-09 20:08:00 -0400

    Added a few things for resource folder

--- comixxer/src/main/java/com/neonyellow/comixxer/controller/HomeController.java
+++ comixxer/src/main/java/com/neonyellow/comixxer/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.neonyellow.comixxer.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
+    @GetMapping
+    public String getIndex(){
+        return "index.html";
+    }
 }
