993fcdc9550098f76e123f0d95e8b9f0742fa3cd
Naim <naimtraore@gmail.com>
2019-04-11 19:40:41 -0400

    link to drawPage

--- comixxr/src/main/java/com/neonyellow/comixxr/controller/UserController.java
+++ comixxr/src/main/java/com/neonyellow/comixxr/controller/UserController.java
@@ -1,4 +1,70 @@
 package com.neonyellow.comixxr.controller;
 
+import com.neonyellow.comixxr.model.Comic;
+import com.neonyellow.comixxr.model.User;
+import com.neonyellow.comixxr.repository.UserRepository;
+import com.neonyellow.comixxr.service.ComixUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private ComixUserDetailsService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value = {"/drawPage"}, method = RequestMethod.GET)
+    public ModelAndView getDrawPage(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("currentUser", user);
+        modelAndView.setViewName("drawPage");
+        return modelAndView;
+    }
+
+    /*GET CURRENT USER CREATIONS FROM DATABASE*/
+    @RequestMapping(value = {"/myCreations"}, method = RequestMethod.GET)
+    public ModelAndView getCreations(){
+
+        return null;
+    }
+
+    /*GET CURRENT USER CURATIONS FROM DATABASE*/
+    @RequestMapping(value = {"/curations"}, method = RequestMethod.GET)
+    public ModelAndView getCurations(){
+
+        return null;
+    }
+
+    /*GET LATEST POSTS OF CURRENT USER'S SUBSCRIPTIONS*/
+    @RequestMapping(value = {"/subscriptions"}, method = RequestMethod.GET)
+    public ModelAndView getSubsciptions(){
+        return null;
+    }
+
+    /*GET USER BROWSE PAGE*/
+    @RequestMapping(value = {"/browse"}, method = RequestMethod.GET)
+    public ModelAndView getBrowse(){
+        return null;
+    }
+
+    /*HELPER FUNCTIONS*/
+    private ArrayList<Comic> getPopularSingles(){
+        return null;
+    }
+
+    private ArrayList<Comic> getPopularSeries(){
+        return null;
+    }
 }
