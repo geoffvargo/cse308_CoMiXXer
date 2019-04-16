e08569479a635b466ab273beadc43ab1c2dc5023
Naim <naimtraore@gmail.com>
2019-04-16 09:17:38 -0400

    Deleted comixxer

--- comixxer/src/main/java/com/neonyellow/comixxer/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.neonyellow.comixxer.repository;
-
-import com.neonyellow.comixxer.model.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-    Role findByRole(String role);
-}
--- comixxer/src/main/java/com/neonyellow/comixxer/service/implementation/UserServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.neonyellow.comixxer.service.implementation;
-
-import com.neonyellow.comixxer.service.UserService;
-
-public class UserServiceImpl implements UserService {
-}
