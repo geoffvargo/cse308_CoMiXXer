bad9905bd0a663ae259d62d7db6db991710c1676
Naim <naimtraore@gmail.com>
2019-04-18 14:33:32 -0400

    clean up

--- comixxr/src/main/java/com/neonyellow/comixxr/service/ComicService.java
+++ comixxr/src/main/java/com/neonyellow/comixxr/service/ComicService.java
@@ -1,8 +1,14 @@
 package com.neonyellow.comixxr.service;
 
+import com.neonyellow.comixxr.model.Comic;
 import com.neonyellow.comixxr.service.interfaces.IComicService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ComicService implements IComicService {
+    public List<Comic> findAllComicsByUserId(Object userId){
+        return null;
+    }
 }
