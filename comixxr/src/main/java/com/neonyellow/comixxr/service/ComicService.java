eca65fc876b45869e8468e44dca3dc0e935705f4
geoffvargo <geoff.vargo@stonybrook.edu>
2019-04-19 22:03:57 -0400

    corrected errors in ComicService class

--- comixxr/src/main/java/com/neonyellow/comixxr/service/ComicService.java
+++ comixxr/src/main/java/com/neonyellow/comixxr/service/ComicService.java
@@ -19,12 +19,22 @@ public class ComicService implements IComicService {
         return comicRepository.findAllByUserId(userId);
     }
 
+    @Override
+    public List<Comic> findAllComicsByUserId(ObjectId userId) {
+        return comicRepository.findAllByUserId(userId);
+    }
+
     public Comic findBy_id(ObjectId id){
         return comicRepository.findBy_id(id);
     }
 
     public void save(Comic comic){
         comicRepository.save(comic);
+    }
+
+    @Override
+    public void deleteComic(ObjectId id) {
+
     }
 
     public void deleteBy_id(ObjectId id) { comicRepository.deleteBy_id(id);}
